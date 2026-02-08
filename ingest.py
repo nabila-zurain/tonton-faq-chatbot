becde00a00d3ce0066479758e5e24846ad29272c
Nabila Zurain MD Yusni <nabila.yusni@qs.com>
2026-02-09 00:34:16 +0800

    Update ingest.py and faiss_index

--- ingest.py
+++ ingest.py
@@ -1,5 +1,5 @@
 # ingest.py
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
